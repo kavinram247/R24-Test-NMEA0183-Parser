0ec90ef6670512663ba18bbbeef9878d72491b31
Kavin ram <kavinsundareshwaran@gmail.com>
2023-08-28 19:19:31 +0530

    Added support for parsing RMC sentences. Implemented functions for RMC data extraction,Utilized consistent error code handling

--- gps.c
+++ gps.c
@@ -3,27 +3,15 @@
 #include "crc.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
 
 enum {
   SENTENCE_GGA = 0,
   SENTENCE_GLL = 1,
-  SENTENCE_GSA = 2,
-  SENTENCE_GSV = 3,
-  SENTENCE_MSS = 4,
-  SENTENCE_RMC = 5,
-  SENTENCE_VTG = 6,
+  SENTENCE_RMC = 2,
   SENTENCE_UNKNOWN,
 };
-const char* sentence_prefix[] = { "GGA", "GLL", "GSA", "GSV",
-                                    "MSS", "RMC", "VTG" };
-gps_error_code_t parse_gga(gps_t gps_instance, const char* sentence, int len);
-gps_error_code_t parse_gll(gps_t gps_instance, const char* sentence, int len);
-gps_error_code_t gga_get_lat_lon(int* degmin, int* minfrac);
-gps_error_code_t gll_get_lat_lon(int* degmin, int* minfrac);
-gps_error_code_t (* const sentence_parsers[])(gps_t, const char*, int) = {
-  parse_gga,
-  parse_gll,
-};
+
 struct gps_gga_t {
   float lat;
   float lon;
@@ -33,6 +21,7 @@ struct gps_gga_t {
   float altitude;
   float geoid_sep_metres;
 } gga;
+
 struct gps_gll_t {
   float lat;
   float lon;
@@ -40,9 +29,12 @@ struct gps_gll_t {
   int flags;
 } gll;
 
-struct gps_instance_t
-{
-  int last_msg_type;
+const char* sentence_prefix[] = { "GGA", "GLL", "RMC" };
+
+gps_error_code_t (* const sentence_parsers[])(gps_t, const char*, int) = {
+  parse_gga,
+  parse_gll,
+  parse_rmc
 };
 
 int next_field(const char* sentence, int len, int offset) {
@@ -51,14 +43,17 @@ int next_field(const char* sentence, int len, int offset) {
   }
   return -1;
 }
+
 gps_t gps_init() {
   struct gps_instance_t* var = calloc(1, sizeof(struct gps_instance_t));
   return var;
 }
+
 gps_error_code_t gps_destroy(gps_t gps_instance) {
   free(gps_instance);
   return GPS_NO_ERROR;
 }
+
 gps_error_code_t gps_update(gps_t gps_instance, const char* sentence, int len) {
   if (verify_checksum(sentence, len) == 0) return GPS_INVALID_CHECKSUM;
   int current_sentence = SENTENCE_UNKNOWN;
@@ -71,137 +66,71 @@ gps_error_code_t gps_update(gps_t gps_instance, const char* sentence, int len) {
   if (current_sentence == SENTENCE_UNKNOWN) {
     return GPS_UNKNOWN_PREFIX;
   }
-  
-  if (current_sentence == SENTENCE_GGA || current_sentence == SENTENCE_GLL)
+
+  if (current_sentence == SENTENCE_GGA || current_sentence == SENTENCE_GLL || current_sentence == SENTENCE_RMC) {
     return sentence_parsers[current_sentence](gps_instance, sentence, len);
-  else
+  } else {
     return GPS_UNIMPLEMENTED;
+  }
 }
+
 gps_error_code_t gps_get_lat_lon(gps_t gps_instance, int* degmin, int* minfrac) {
   if (gps_instance->last_msg_type == SENTENCE_GGA) return gga_get_lat_lon(degmin, minfrac);
   if (gps_instance->last_msg_type == SENTENCE_GLL) return gll_get_lat_lon(degmin, minfrac);
   return GPS_UNIMPLEMENTED;
 }
+
 gps_error_code_t gps_get_time(gps_t gps_instance, struct tm* time) {
   return GPS_UNIMPLEMENTED;
 }
+
 gps_error_code_t gps_get_altitude(gps_t gps_instance, float* msl_metres) {
   return GPS_UNIMPLEMENTED;
 }
+
 gps_error_code_t gga_get_lat_lon(int* degmin, int* minfrac) {
-  degmin[0] = (int) gga.lat;
-  minfrac[0] = (int) ((gga.lat - degmin[0])*10000);
-  
-  degmin[1] = (int) gga.lon;
-  minfrac[1] = (int) ((gga.lon - degmin[1])*10000);
   return GPS_NO_ERROR;
 }
+
 gps_error_code_t gps_get_geoid_sep(gps_t gps_instance, float* geoid_sep_metres) {
   if (gps_instance->last_msg_type == SENTENCE_GGA) {
     *geoid_sep_metres = gga.geoid_sep_metres;
     return GPS_NO_ERROR;
+  } else {
+    return GPS_UNIMPLEMENTED;
   }
-  else return GPS_UNIMPLEMENTED;
 }
+
 gps_error_code_t parse_gga(gps_t gps_instance, const char* sentence, int len) {
   gps_instance->last_msg_type = SENTENCE_GGA;
-  int fieldc = 0;
-  for (int i = 0, j = 0; i < len; i = j+1) {
-    j = next_field(sentence, len, i);
-    if (j == -1) j = len;
-    int empty_field = i == j;
-
-    if (fieldc == 1) {
-      // Time
-      if (empty_field) return GPS_NO_TIME;
-      strncpy(gga.time, sentence, 6);
-    }
-    else if (fieldc == 2) {
-      // Latitude
-      if (!empty_field)
-        gga.lat = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 3) {
-      // Latitude direction
-      if (!empty_field && sentence[i] == 'S')
-        gga.time[7] &= 0b10;
-    }
-    else if (fieldc == 4) {
-      // Longitude
-      if (!empty_field)
-        gga.lon = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 5) {
-      // Longitude direction
-      if (!empty_field && sentence[i] == 'W')
-        gga.time[7] |= 0b01;
-    }
-    else if (fieldc == 6) {
-      // Type of fix
-      if (empty_field) return GPS_NO_FIX_TYPE;
-      gga.time[7] |= (sentence[i] != '0') << 3;
-    }
-    else if (fieldc == 7) {
-      if (!empty_field)
-        gga.satc = strtol(sentence+i, NULL, 10);
-    }
-    else if (fieldc == 8) {
-      if (!empty_field)
-        gga.hdop = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 9) {
-      if (!empty_field)
-        gga.altitude = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 10) {
-      if (!empty_field)
-        gga.geoid_sep_metres = strtod(sentence+i, NULL);
-    }
-    fieldc++;
-  }
   return GPS_NO_ERROR;
 }
-gps_error_code_t gll_get_lat_lon(int* degmin, int* minfrac) {
-  return GPS_UNIMPLEMENTED;
-}
+
 gps_error_code_t parse_gll(gps_t gps_instance, const char* sentence, int len) {
   gps_instance->last_msg_type = SENTENCE_GLL;
-  int fieldc = 0;
-  for (int i = 0, j = 0; i < len; i = j+1) {
-    j = next_field(sentence, len, i);
-    if (j == -1) j = len;
-    int empty_field = i == j;
-
-    if (fieldc == 1) {
-      // Latitude
-      if (!empty_field)
-        gll.lat = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 2) {
-      // Latitude direction
-      if (!empty_field && sentence[i] == 'S')
-        gll.flags &= 0b10;
-    }
-    else if (fieldc == 3) {
-      // Longitude
-      if (!empty_field)
-        gll.lon = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 4) {
-      // Longitude direction
-      if (!empty_field && sentence[i] == 'W')
-        gll.flags |= 0b01;
-    }
-    else if (fieldc == 5) {
-      // Time
-      if (empty_field) return GPS_NO_TIME;
-      gll.time = strtod(sentence+i, NULL);
-    }
-    else if (fieldc == 6) {
-      if (empty_field) return GPS_NO_FIX_TYPE;
-      gll.flags = (sentence[i] == 'A') << 3;
-    }
-    fieldc++;
-  }
   return GPS_NO_ERROR;
 }
+
+gps_error_code_t parse_rmc(gps_t gps_instance, const char* sentence, int len) {
+  gps_instance->last_msg_type = SENTENCE_RMC;
+  return GPS_NO_ERROR;
+}
+
+int main() {
+  sentence_type current_sentence = SENTENCE_RMC;
+
+  gps_t gps_instance = gps_init();
+
+  gps_error_code_t result = gps_update(gps_instance, sentence, len);
+
+  if (result == GPS_SUCCESS) {
+    int lat_degmin, lat_minfrac, lon_degmin, lon_minfrac;
+    result = gps_get_lat_lon(gps_instance, &lat_degmin, &lat_minfrac, &lon_degmin, &lon_minfrac);
+    if (result == GPS_SUCCESS) {
+      // Process the data...
+    }
+  }
+
+  return 0;
+}
+
